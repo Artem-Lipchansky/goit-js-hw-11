95ac7fc953d76a3de993a14a12795b70c9ba53c1
Artem Lipchansky <lipchanskiy.an@gmail.com>
2024-02-24 22:39:06 +0200

    added

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,51 @@
+import { renderImages } from './render-functions';
+
+export async function submitSearch() {
+  const searchInput = document.querySelector('.searchInput');
+  const query = searchInput.value.trim();
+  const loader = document.querySelector('.loader');
+  const galleryContainer = document.querySelector('.gallery');
+
+  if (query === '') {
+    iziToast.show({
+      message: 'Please enter a keyword',
+      backgroundColor: 'yellow',
+    });
+    return Promise.reject('Empty query');
+  }
+
+  const apiKey = '42546986-414d01ac2526fb250e7f0e208';
+  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  loader.style.display = 'inline-block';
+
+  galleryContainer.innerHTML = '';
+
+  return fetch(apiUrl)
+    .then(response => {
+      loader.style.display = 'none';
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          color: '#fafafb',
+          backgroundColor: '#ef4040',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      } else {
+        renderImages(data.hits);
+      }
+    })
+    .catch(error => {
+      loader.style.display = 'none';
+      iziToast.error({
+        title: 'Error!',
+        message: `An error occurred while fetching data: ${error.message}. Please try again later.`,
+      });
+    });
+}
