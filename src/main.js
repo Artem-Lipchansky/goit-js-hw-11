8799deb891a71e2a43f79d082e8fe2c5d9cd48b7
Artem Lipchansky <lipchanskiy.an@gmail.com>
2024-02-25 22:00:41 +0200

    added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+
 import { submitSearch } from './js/pixabay-api';
 
 const searchForm = document.querySelector('.searchForm');
